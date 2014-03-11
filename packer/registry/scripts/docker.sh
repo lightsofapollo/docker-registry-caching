808f2c70121993b1fb8b34b4ac0aa1a2056f8d7b
James Lal <james@lightsofapollo.com>
2014-03-11 16:17:09 -0700

    Packer hacks

---
+++ packer/registry/scripts/docker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+
+# Add the Docker repository to your apt sources list.
+sudo sh -c "echo deb http://get.docker.io/ubuntu docker main\
+> /etc/apt/sources.list.d/docker.list"
+
+# add docker group and add vagrant to it
+sudo groupadd docker
+sudo usermod -a -G docker $USER
+
+# install curl
+sudo apt-get update
+sudo apt-get install -y -q curl
+
+# add the docker gpg key
+sudo curl https://get.docker.io/gpg > /tmp/docker-gpg
+sudo apt-key add /tmp/docker-gpg
+
+# Update your sources
+sudo apt-get update
+sudo apt-get upgrade -y
+
+# Install. Confirm install.
+sudo apt-get install -y -q cgroup-lite lxc-docker
---
+++ packer/registry/scripts/preload.sh
@@ -0,0 +1,2 @@
+#!/bin/bash -e
+sudo docker pull blalor/s3-registry
